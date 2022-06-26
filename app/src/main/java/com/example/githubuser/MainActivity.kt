310f6519a5770525f6457d16e8740564c1dd62ff
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-06-26 14:54:00 +0700

    create card user

--- app/src/main/java/com/example/githubuser/MainActivity.kt
+++ app/src/main/java/com/example/githubuser/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.githubuser
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val img: ImageView = findViewById(R.id.img_item_photo)
+        Glide.with(this)
+            .load(R.drawable.user1)
+            .circleCrop()
+            .into(img)
     }
 }
