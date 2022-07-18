ae703a836253a8b11343f4403578fe64599cc1c5
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-07-18 20:17:38 +0700

    ### Create user detail view model

---
+++ app/src/main/java/com/example/githubuser/UserDetailViewModel.kt
@@ -0,0 +1,43 @@
+package com.example.githubuser
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class UserDetailViewModel: ViewModel() {
+    private val _userDetail = MutableLiveData<UserDetailResponse>()
+    val userDetail: LiveData<UserDetailResponse> = _userDetail
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    private val _isError = MutableLiveData<Boolean>()
+    val isError: LiveData<Boolean> = _isError
+
+    fun getUserDetail(username: String) {
+        _isLoading.value = true
+        val client = ApiConfig.getApiService().getUserDetail(username)
+        client.enqueue(object : Callback<UserDetailResponse> {
+            override fun onResponse(
+                call: Call<UserDetailResponse>,
+                response: Response<UserDetailResponse>
+            ) {
+                _isLoading.value = false
+                if (response.isSuccessful) {
+                    _isError.value = false
+                    _userDetail.value = response.body()
+                } else {
+                    _isError.value = true
+                }
+            }
+
+            override fun onFailure(call: Call<UserDetailResponse>, t: Throwable) {
+                _isLoading.value = false
+                _isError.value = true
+            }
+        })
+    }
+}
