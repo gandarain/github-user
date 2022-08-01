92812b602ac5e5d825664ac28228760138206a1e
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-08-01 22:31:21 +0700

    ### Setup View Model Factory for Favorite User

---
+++ app/src/main/java/com/example/githubuser/viewModel/ViewModelFactory.kt
@@ -0,0 +1,34 @@
+package com.example.githubuser.viewModel
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.githubuser.ui.detail.UserDetailViewModel
+import com.example.githubuser.ui.favorites.FavoritesViewModel
+
+class ViewModelFactory private constructor(private val mApplication: Application) : ViewModelProvider.NewInstanceFactory() {
+    companion object {
+        @Volatile
+        private var INSTANCE: ViewModelFactory? = null
+
+        @JvmStatic
+        fun getInstance(application: Application): ViewModelFactory {
+            if (INSTANCE == null) {
+                synchronized(ViewModelFactory::class.java) {
+                    INSTANCE = ViewModelFactory(application)
+                }
+            }
+            return INSTANCE as ViewModelFactory
+        }
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(FavoritesViewModel::class.java)) {
+            return FavoritesViewModel(mApplication) as T
+        } else if(modelClass.isAssignableFrom(UserDetailViewModel::class.java)) {
+            return UserDetailViewModel(mApplication) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
+    }
+}
